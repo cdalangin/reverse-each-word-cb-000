bb78762cfaa1bb5ff95a0027d107b279ca067f27
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-19 01:42:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,7 @@ def reverse_each_word(phrase)
   separate = phrase.split(" ")
   reversed = []
   separate.each do |word|
-    word.split("").reverse
-
-    reversed << word
+    reversed <<     word.split("").reverse
   end
   reversed
 end
